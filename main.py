5b70a2130f338c1ff941a32bdfe187bc9b55156c
Alex <98416718+alexandru-ghibea@users.noreply.github.com>
2022-05-31 15:44:35 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+from turtle import Screen
+from snake import Snake
+import time
+from food import Food
+from scoreboard import ScoreBoard
+
+#TODO 0: Set up the Screen
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = ScoreBoard()
+#TODO 3. Control the Snake
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+game_one = True
+while game_one:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+    #TODO 4 Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+    # TODO 6 Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_one = False
+        scoreboard.game_over()
+    #TODO 7 collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_one = False
+            scoreboard.game_over()
+screen.exitonclick()
+
---
+++ raw.py
@@ -0,0 +1,41 @@
+#TODO LIST for Snake Game:
+    #TODO 0: Setup screen
+    #TODO 1. Create Snake Body
+    #TODO 2. Move the Snake
+    #TODO 3. Control the Snake
+    #TODO 4. Detect collision with food
+    #TODO 5. Create Scoreboard
+    #TODO 6. Detect collision with wall
+    #TODO 7. Detect collision with tail/body of the snake
+
+#TODO 0: Set up the Screen
+from turtle import Screen, Turtle
+import time
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+segments = []
+
+#TODO 1. Create Snake Body
+for position in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(position)
+    segments.append(new_segment)
+
+game_one = True
+while game_one:
+    screen.update()
+    time.sleep(0.1)
+# TODO 2. Move the Snake
+    for seg_num in range(len(segments)-1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
+    segments[0].left(90)
+screen.exitonclick()
